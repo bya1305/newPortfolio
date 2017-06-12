07106f866becbf43e2909d1b11ed7f3ee28501d3
Josh Avina <josh.avina78@gmail.com>
2017-06-11 22:18:41 -0700

    added defaults and custom scope

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,13 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumbnail
+  after_initialize :set_defaults
+
+  def self.angular
+    where(subtitle: "Angular")
+  end
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumbnail ||= "http://via.placeholder.com/350x200"
+  end
 end
