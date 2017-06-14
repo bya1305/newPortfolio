90e94c026f7c6f9506e0747cc15abe98d72730f8
Josh Avina <josh.avina78@gmail.com>
2017-06-13 17:36:43 -0700

    added nested attributes for portfolio model and extended the fields on form for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def show
@@ -51,7 +52,7 @@ class PortfoliosController < ApplicationController
   private
 
     def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
     end
 
     def set_portfolio
